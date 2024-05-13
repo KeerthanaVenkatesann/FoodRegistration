933a929a1849c66ac285eea78131c3ee0e0efcfe
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-05-13 11:52:43 +0530

    startfromtoday

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Form from './Components/Food Registration/Form';
+import List from './Components/Food Registration/List';
+import Edit from './Components/Food Registration/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/form'element={<Form/>}></Route>
+      <Route path='/list'element={<List/>}></Route>
+      <Route path='/edit'element={<Edit/>}></Route>
+    </Routes>
+      </BrowserRouter>
   );
 }
 
